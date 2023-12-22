127c69683c6e94a1171dd1abeca4dd068ae36b5d
Dipendra km <dipendrakm703@gmail.com>
2023-12-22 15:20:46 -0500

    last

--- lms/settings.py
+++ lms/settings.py
@@ -133,7 +133,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'mmmddd.dk@gmail.com'
-EMAIL_HOST_PASSWORD = 'D@ni$h703'
+EMAIL_HOST_PASSWORD = 'swjd miah pedw bjwe'
 
 
 
